e8fd2a9a6623fd952f134de8f3752e1a33f17eb7
Den <36740786+dengl91@users.noreply.github.com>
2021-10-13 11:41:05 +0300

    Update register_block.php

--- register_block.php
+++ register_block.php
@@ -4,12 +4,12 @@ function nuoptima_acf_init() {
 	// check function exists
 	if ( function_exists('acf_register_block') ) {
         acf_register_block(array(
-            'name'				=> 'faq',
-            'title'				=> __('FAQ item'),
-            'description'		=> __('FAQ item with schema org'),
-            'render_callback'	=> 'nuoptima_block_render_callback',
-            'category'			=> 'homepage',
-            'icon'				=> 'format-status'
+            'name'            => 'faq',
+            'title'           => __('FAQ item'),
+            'description'     => __('FAQ item with schema org'),
+            'render_callback' => 'nuoptima_block_render_callback',
+            'category'        => 'homepage',
+            'icon'            => 'format-status'
         ));
     }
 
